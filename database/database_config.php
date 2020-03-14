55eece4355c31c4aa2fd8b541a1224a9c7541c74
Pedro Haramoto <pedroharamoto@gmail.com>
2020-03-14 08:47:32 -0300

    database_config.php: it was added the methods for creating the tables 'user' and 'user_drink'

--- database/database_config.php
+++ database/database_config.php
@@ -1,7 +1,7 @@
 <?php
 // File created by Pedro Haramoto on 14/03/2020
 //
-// This file defines the database's class
+// This file defines the database's class and its methods
 //
 
 // Creating the Database Class
@@ -25,7 +25,7 @@ class Database{
             $this->conn = new PDO("mysql:host=".$this->host, $this->username, $this->password);
             $this->conn->exec("SET NAMES utf8");
             //
-            $query  = "CREATE DATABASE " . $this->db_name; // SQL Query to create a database named by the public variable $db_name
+            $query  = "CREATE DATABASE IF NOT EXISTS" . $this->db_name; // SQL Query to create a database named by the public variable $db_name
             //
             $stmt   = $this->conn->prepare($query); // Prepares the query to execute it
             $stmt->execute(); //executes the query
@@ -38,6 +38,78 @@ class Database{
         //Connection successful
         return $this->conn;
     }
+    //
+    // This method gets the datebase connetion
+    //
+    public function getConnection(){
+        //
+        $this->conn = null; //sets the public variable $conn as null in order to clean it
+
+        try{
+            //tries the connection
+            // Using PDO in order to use an expection class that handles possible problems that may occur
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);            $this->conn->exec("SET NAMES utf8");
+            //
+            $this->conn->exec("set names utf8");
+        }
+        catch(PDOException $exception){
+            //if was not possible to connect to the database, then shows the following message
+            echo "Connection to the " . $this->db_name . " has failed<br>Error: " . $exception->getMessage();
+            //
+        }
+        //Connection successful
+        return $this->conn;
+    }
+    //
+    // This method creates the user table
+    //
+    public function createUserTable(){
+        //
+        $this->getConnection();
+        //
+        $query = "
+                    CREATE TABLE IF NOT EXISTS `user` (
+                      `user_id` int(11) NOT NULL AUTO_INCREMENT,
+                      `user_name` varchar(250) NOT NULL,
+                      `user_email` varchar(250) NOT NULL,
+                      `user_password` varchar(2048) NOT NULL,
+                      PRIMARY KEY (`user_id`)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1;
+        ";
+        //
+        $stmt = $this->conn->prepare($query); // Prepares the query to execute it
+        $stmt->execute(); //executes the query
+        //
+    }
+    //
+    // This method creates the user table
+    //
+    public function createUserDrinkTable(){
+        //
+        $this->getConnection();
+        //
+        $query = "
+                    CREATE TABLE IF NOT EXISTS `user_drink` (
+                      `user_drink_id` int(11) NOT NULL AUTO_INCREMENT,
+                      `user_id` int(11) NOT NULL,
+                      `user_drink_ml` float NOT NULL,
+                      `user_drink_date` timestamp NOT NULL DEFAULT current_timestamp(),
+                      PRIMARY KEY (`user_drink_id`)
+                    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 AUTO_INCREMENT=1;
+
+                    ALTER TABLE `user_drink`
+                        ADD KEY `user_id` (`user_id`);
+
+                    ALTER TABLE `user_drink`
+                        ADD CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`);
+
+        ";
+        //
+        $stmt = $this->conn->prepare($query); // Prepares the query to execute it
+        $stmt->execute(); //executes the query
+        //
+    }
 }
+
 
 ?>
