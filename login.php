0fb446eb60d633b5a4784035722cc8c7dcbf31f6
Pedro Haramoto <pedroharamoto@gmail.com>
2020-03-14 21:45:29 -0300

    Login authentication OK

---
+++ login.php
@@ -0,0 +1,82 @@
+<?php
+//
+// file created by Pedro Haramoto on 14/03/2020
+// this file sets all the user/login actions
+//
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+//
+include_once 'database/database_config.php';
+include_once 'objects/object_user.php';
+//includes all jwt lib
+require "vendor/autoload.php";
+use \Firebase\JWT\JWT;
+//
+$database   = new Database();
+$db         = $database->getConnection();
+//
+$user       = new User($db);
+// captures the posted data
+$data   = json_decode(file_get_contents("php://input"));
+//
+$method = $_SERVER['REQUEST_METHOD'];
+// setting user property values
+$user->user_email       = $data->user_email;
+$user->user_password    = $data->user_password;
+//
+if( ($user->existsUserByEmail()) && (password_verify($data->user_password,$user->user_password)) ){
+    //
+    // the user exists and the verification is ok
+    //
+    // creating token
+    $key = "THE_SECRET_KEY";
+    $iss = "THE_ISSUER"; // server name
+    $aud = "THE_AUDIENCE";
+    $iat = time(); // issued at xx:xx
+    $nbf = $iat + 10; //not before in seconds
+    $exp = $iat + 180; // expire time in seconds
+    //
+    $token = array(
+        "iss"   => $iss,
+        "aud"   => $aud,
+        "iat"   => $iat,
+        "nbf"   => $nbf,
+        "exp"   => $exp,
+        "data"  => array(
+            "user_id"       => $user->user_id,
+            "user_name"     => $user->user_name,
+            "user_email"    => $user->user_email,
+            "user_password" => $user->user_password
+        )
+    );
+    //
+    http_response_code(200); // OK
+    //
+    $jwt = JWT::encode($token,$key);
+    //
+    echo json_encode(array(
+        "message"       => "Login OK " . $user->user_name,
+        "jwt"           => $jwt,
+        "iduser"        => $user->user_id,
+        "email"         => $user->user_email,
+        "name"          => $user->user_name,
+        "drink_counter" => $user->countTimesDrink()
+    ));
+}
+else{
+    //
+    http_response_code(401); //ERROR
+    //
+    if(!($user->existsUserByEmail()))
+        echo json_encode(array("message"   => "Login has failed. User doesn't exist"));
+    else
+        echo json_encode(array("message"   => "Login has failed. Wrong password"));
+
+}
+
+
+
+?>
--- user.php
+++ user.php
@@ -5,7 +5,7 @@
 //
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Allow-Methods: *"); // allow all the methods
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 //
@@ -18,14 +18,35 @@ $db         = $database->getConnection();
 $user       = new User($db);
 // captures the posted data
 $data   = json_decode(file_get_contents("php://input"));
+//
+$method = $_SERVER['REQUEST_METHOD'];
 // setting user property values
 $user->user_name        = $data->user_name;
 $user->user_email       = $data->user_email;
 $user->user_password    = $data->user_password;
 //
-if( ($user->user_name) and ($user->user_email) and ($user->user_password) and ($user->newUser()) ){
+// the following code defines the actions
+/*
+according to the .htaccess{
+    action = 1 => New user;
+    action = 2 => Return all the users;
+    action = 3 => return the specified iduser
+    action = 4 => edit the specified iduser
+    action = 5 => delete the specified iduser
+    action = 6 => count how many times the specified iduser drank water
+    action = 7 => login
+}
+*/
+//
+// New User
+if( ($user->user_name) && ($user->user_email) && ($user->user_password) && ($user->newUser()) ){
     // if the conditions are true, then the user will be created
+    http_response_code(200); // OK
     echo json_encode(array("message" => "User " . $user->user_email . " was created."));
 }
-//
+else {
+    http_response_code(400); // Error
+    //
+    echo json_encode(array("message" => "It was not possible to register a new user."));
+}
 ?>
